9e717f123367e5e3cc58273704368438289aaa92
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 14:22:04 -0500

    add code and pass initial test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, numTimes) {
+    let repetitions = parseInt(numTimes);
 
+    if(repetitions < 0)
+        return('ERROR');
+        
+    let output = '';
+    for(let i = 0; i < repetitions; i++){
+        output += text;
+    }
+    return output;
 };
 
 // Do not edit below this line
