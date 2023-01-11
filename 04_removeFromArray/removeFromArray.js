c25da9f5362e84090d8ceb72e9787ce3783036b7
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 15:29:43 -0500

    passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let theArray = array;
 
-};
+    for(const arg of args){
+        let index = theArray.indexOf(arg);
+        if( index >= 0 ){
+            theArray.splice(index, 1);
+        }
+    }
+    return theArray;
+}; 
 
-// Do not edit below this line
 module.exports = removeFromArray;
