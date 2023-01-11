a4c7d2b3b0ef7d1e896e0906996cb6cdbd4ac862
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 16:26:21 -0500

    passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,28 +1,41 @@
-const add = function() {
-	
+const add = function(first, second) {
+    return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+
+    for(const number of numbers)
+      sum += number;
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  
+  for(const number of numbers){
+    product *= number;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+
+	for(let i = 1; i <= number; i++){
+    factorial *= i;
+  }
+  return factorial;
 };
 
-// Do not edit below this line
 module.exports = {
   add,
   subtract,
