7389f47ecd99b7ab1769c31f63f07887ff7e4ed4
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 16:11:50 -0500

    passed tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  return Number(((F - 32) * 5/9).toFixed(1));
+}
+
+const convertToFahrenheit = function(C) {
+  return Number(((C * 1.8) + 32).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
-};
-
-// Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
