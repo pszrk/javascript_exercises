f71803fdabaf03131a950e0d21d504f07a00134d
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 16:48:35 -0500

    passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    //reverse the string, and convert to lowercase, letters only
+    let reversed = '';
+    let temp = string.split('');
+    for(let i = temp.length - 1; i >= 0; i--)
+        // check if it is a letter
+        if(temp[i].toUpperCase() != temp[i].toLowerCase())
+            reversed += temp[i].toLowerCase();
+
+    //convert initial string to lowercase, letters only
+    let forward = '';
+    for(let i = 0; i < string.length; i++)
+        //  check if it is a letter
+        if(string.charAt(i).toLowerCase() != string.charAt(i).toUpperCase())
+            forward += string.charAt(i).toLowerCase();
+
+    //compare the forward string with the reversed string
+    console.log(forward + '\n' +reversed);
+    return (forward === reversed);
 };
 
-// Do not edit below this line
 module.exports = palindromes;
