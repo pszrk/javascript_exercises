e5946d1a15e6fc1ecd3fd14700e4ff1592548efc
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 18:01:20 -0500

    passed tests

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,57 @@
-const caesar = function() {
+const caesar = function(string, shift) {
 
+    let encryptedString = '';
+
+    for(let i = 0 ; i < string.length; i++){
+
+        //  get unicode value of current letter in the string
+        let unicode = string.charCodeAt(i);
+        let newcode = unicode;
+
+        //  if it is an uppercase letter
+        if(unicode >= 65 && unicode <= 90){
+            //  for positive shift
+            if( shift > 0){
+                for(let i = 0; i < shift; i++){
+                    newcode++;
+                    if (newcode > 90)
+                        newcode = 65;
+                }
+            }
+            //  for negative shift
+            else if (shift < 0){
+                for(let i = 0; i > shift; i--){
+                    newcode--;
+                    if (newcode < 65)
+                        newcode = 90;
+                }
+            }
+        }
+
+        //  otherwise, if it is a lowercase letter
+        else if(unicode >= 97 && unicode <= 122){
+            //  for positive shift
+            if( shift > 0){
+                for(let i = 0; i < shift; i++){
+                    newcode++;
+                    if (newcode > 122)
+                        newcode = 97;
+                }
+            }
+            //  for negative shift
+            else if (shift < 0){
+                for(let i = 0; i > shift; i--){
+                    newcode--;
+                    if (newcode < 97)
+                        newcode = 122;
+                }
+            }
+        }
+
+        encryptedString += String.fromCharCode(newcode);
+    }
+
+    return encryptedString;
 };
 
-// Do not edit below this line
 module.exports = caesar;
