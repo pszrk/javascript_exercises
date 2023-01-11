9427c207f149a909ea626f837140408a0e290673
pszrk <120336989+pszrk@users.noreply.github.com>
2023-01-11 17:14:41 -0500

    passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let number = parseInt(index);
+    if (number < 1)
+        return ('OOPS');
+    
+    let fiboArray = [1, 1];
+    let length = fiboArray.length;
+    while(length < index){
+        fiboArray.push(fiboArray[length - 1] + fiboArray[length - 2]);
+        length = fiboArray.length;
+    }
+    return fiboArray[length - 1];
 };
 
-// Do not edit below this line
+console.log(fibonacci(-3));
+
 module.exports = fibonacci;
